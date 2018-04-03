30effba7993992957e9bbf649ae06ea3b43b712f
Ari <dominiquearia0@gmail.com>
2018-04-03 12:04:08 -0700

    migration up for trips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403165022) do
+ActiveRecord::Schema.define(version: 20180403185124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20180403165022) do
   create_table "passengers", force: :cascade do |t|
     t.string "name"
     t.string "phone_num"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.date "date"
+    t.integer "rating"
+    t.integer "cost"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
