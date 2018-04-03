1b02c449977ce2afc57f93601aaad3034b0ff9fd
Ari <dominiquearia0@gmail.com>
2018-04-03 09:52:40 -0700

    Added Passenger & Driver models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180403165022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "drivers", force: :cascade do |t|
+    t.string "name"
+    t.string "vin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_num"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
