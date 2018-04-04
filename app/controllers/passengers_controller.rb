6989de99468794be6b296c000811a24b517fe971
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-04-03 21:51:44 -0700

    Implemented Destroy Method for Passenger and also included List of Trips taken in Show method

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -12,4 +12,31 @@ class PassengersController < ApplicationController
     @passenger = Passenger.find(passenger_id)
   end
 
+  def edit
+    @passenger = Passenger.find(params[:id])
+  end
+
+  def update
+    @passenger = Passenger.find(params[:id])
+
+    @passenger.assign_attributes(pass_params)
+
+    if @passenger.save
+      redirect_to passenger_path(passenger)
+    end
+  end
+
+  def destroy
+    @passenger = Passenger.find(params[:id])
+
+    @passenger.destroy
+
+    redirect_to '/passengers'
+    flash[:alert] = "Passenger deleted"
+  end
+
+  private
+  def pass_params
+    params.require(:passenger).permit(:name, :phone_num)
+  end
 end
