5c2deff9cc7ed34a1b0f5421c68bb7767f3c7e75
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-04-05 14:50:06 -0700

    Can now edit trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -12,6 +12,14 @@ class TripsController < ApplicationController
     @trip = Trip.find(params[:id])
   end
 
+  def update
+    trip = Trip.find(params[:id])
+    trip.assign_attributes(trip_params)
+    if trip.save
+      redirect_to trip_path(trip)
+    end
+  end
+
   def new
     @trip = Trip.new(passenger_id: params[:passenger_id])
   end
