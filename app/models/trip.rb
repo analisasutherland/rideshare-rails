b1dc5ded6cb660db455ad3f287d779ecee2e4938
Ari <dominiquearia0@gmail.com>
2018-04-05 15:33:14 -0700

    small changes to rating on trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,12 @@ class Trip < ApplicationRecord
 
   validates :passenger_id, presence: true
   validates :driver_id, presence: true
+
+  # def rate_trip
+  #   unrated_trips = self.trips.where(rating: " ")
+  #   if unrated_trips.length == 0
+  #     return nil
+  #   end
+  # end
+
 end
